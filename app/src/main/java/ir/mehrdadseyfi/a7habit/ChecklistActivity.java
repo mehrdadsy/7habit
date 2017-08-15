e85910aeb872baece09145132656d29dcdfebbc7
mehrdad <mehrdad.seyfi@ymail.com>
2017-08-15 14:58:09 +0430

    mehrdad

---
+++ app/src/main/java/ir/mehrdadseyfi/a7habit/ChecklistActivity.java
@@ -0,0 +1,13 @@
+package ir.mehrdadseyfi.a7habit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChecklistActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checklist);
+    }
+}
