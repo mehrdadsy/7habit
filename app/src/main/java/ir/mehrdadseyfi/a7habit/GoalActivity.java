3e607855420ba2b1fb2e1f29a7ef73814cae5600
mehrdad <mehrdad.seyfi@ymail.com>
2017-08-15 17:32:33 +0430

    mehrdad

--- app/src/main/java/ir/mehrdadseyfi/a7habit/GoalActivity.java
+++ app/src/main/java/ir/mehrdadseyfi/a7habit/GoalActivity.java
@@ -1,14 +1,34 @@
 package ir.mehrdadseyfi.a7habit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class GoalActivity extends AppCompatActivity {
-
+    ImageView long_goal;
+    ImageView short_goal;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_goal);
+        long_goal=(ImageView)findViewById(R.id.long_goal);
+        short_goal=(ImageView)findViewById(R.id.short_goal);
+        long_goal.setImageResource(R.drawable.hadaf_boland);
+       short_goal.setImageResource(R.drawable.hadaf_kootah);
+            long_goal.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(new Intent(GoalActivity.this, LongGoalActivity.class));
+                }
+            });
+        short_goal.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(new Intent(GoalActivity.this, ShotGoalActivity.class));
+                }
+            });
 
     }
 }
---
+++ app/src/main/java/ir/mehrdadseyfi/a7habit/LongGoalActivity.java
@@ -0,0 +1,18 @@
+package ir.mehrdadseyfi.a7habit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class LongGoalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_long_goal);
+    }
+}
